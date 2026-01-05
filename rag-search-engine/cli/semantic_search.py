643f81e6105dd162d2ebe90e55276f79f4321df1
runner <runner@berkeley.edu>
2026-01-04 16:13:39 -0800

    wip

--- rag-search-engine/cli/semantic_search.py
+++ rag-search-engine/cli/semantic_search.py
@@ -1,9 +1,18 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
+import json
+from typing import Any
+from pathlib import Path
 
 
 class SemanticSearch:
+    MOVIE_EMBEDDINGS_FILE = "cache/movie_embeddings.npy"
+
     def __init__(self):
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
 
     def generate_embedding(self, text: str):
         if not text.strip():
@@ -11,6 +20,37 @@ class SemanticSearch:
 
         embeddings = self.model.encode([text])
         return embeddings[0]
+
+    def build_embeddings(self, documents: list[dict]) -> list:
+        self.documents = documents
+
+        docs = []
+        for d in documents:
+            self.document_map[d["id"]] = d
+            docs.append(f"{d['title']}: {d['description']}")
+
+        self.embeddings = self.model.encode(docs, show_progress_bar=True)
+        np.save(self.MOVIE_EMBEDDINGS_FILE, self.embeddings)
+
+        return self.embeddings
+
+    def load_or_create_embeddings(self, documents: list[dict]):
+        self.documents = documents
+
+        docs = []
+        for d in documents:
+            self.document_map[d["id"]] = d
+            docs.append(f"{d['title']}: {d['description']}")
+
+        file_path = Path(self.MOVIE_EMBEDDINGS_FILE)
+        if file_path.exists():
+            self.embeddings = np.load(self.MOVIE_EMBEDDINGS_FILE)
+            if len(self.embeddings) != len(self.documents):
+                self.build_embeddings(documents)
+        else:
+            self.build_embeddings(documents)
+
+        return self.embeddings
 
 
 def verify_model():
@@ -26,3 +66,20 @@ def embed_text(text):
     print(f"Text: {text}")
     print(f"First 3 dimensions: {embedding[:3]}")
     print(f"Dimensions: {embedding.shape[0]}")
+
+
+def verify_embeddings():
+    search = SemanticSearch()
+
+    with open("data/movies.json") as f:
+        json_data: dict[str, list[dict[str, Any]]] = json.load(f)
+        movies = json_data["movies"]
+        search.load_or_create_embeddings(movies)
+
+    documents = search.documents
+    embeddings = search.embeddings
+
+    print(f"Number of docs:   {len(documents)}")
+    print(
+        f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions"
+    )
--- rag-search-engine/cli/semantic_search_cli.py
+++ rag-search-engine/cli/semantic_search_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from semantic_search import verify_model, embed_text
+from semantic_search import verify_embeddings, verify_model, embed_text
 
 
 def main():
@@ -9,6 +9,7 @@ def main():
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     subparsers.add_parser("verify", help="Verify semantic search model")
+    subparsers.add_parser("verify_embeddings", help="Verify semantic search embeddings")
 
     embed_parser = subparsers.add_parser("embed_text", help="Embed text")
     embed_parser.add_argument("text", type=str)
@@ -18,6 +19,8 @@ def main():
     match args.command:
         case "embed_text":
             embed_text(args.text)
+        case "verify_embeddings":
+            verify_embeddings()
         case "verify":
             verify_model()
         case _:
